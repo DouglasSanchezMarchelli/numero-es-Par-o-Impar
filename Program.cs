17ae4ac117f61c9a701966449152d3ea0625e2b1
Douglas Sánchez <142137936+DouglasSanchezMarchelli@users.noreply.github.com>
2023-08-14 15:02:51 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NumeroParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+Console.WriteLine("Determinar si un numero es Par o Impar");
+
+Console.Write("Ingrese un número entero: ");
+int numero = int.Parse(Console.ReadLine());
+
+ if (numero % 2 == 0)
+ {
+ Console.WriteLine($"El número {numero} es par.");
+ }
+ else
+ {
+ Console.WriteLine($"El número {numero} es impar.");
+   }
+  }
+ }
+}
